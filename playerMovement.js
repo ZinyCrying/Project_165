dab857d7e68d7f39301d0f1a0e771743a89694ac
ZinyCrying <112892791+ZinyCrying@users.noreply.github.com>
2023-12-11 20:12:15 +0530

    Add files via upload

---
+++ playerMovement.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent("player-movement", {
+    init: function(){
+        this.walk()
+    },
+    walk: function(){
+        window.addEventListener("keydown", e => {
+            if(e.key === "w" || e.key === "s" || e.key === "a" || e.key === "d"){
+                var entity = document.querySelector("#sound2")
+                entity.components.sound.playSound()
+            }
+        })
+    }
+})
